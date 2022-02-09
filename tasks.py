b7da19325483437fc6c8cb4ccdc8f65807aa5c51
Samira <samirehuseyinli@gmail.com>
2022-02-09 11:13:48 +0400

    find max_revenue_mobile

--- tasks.py
+++ tasks.py
@@ -7,3 +7,12 @@
 def find_max_revenue(cursor):
     cursor.execute("SELECT id FROM Transactions ORDER BY revenue DESC")
     return cursor.fetchone()[0]
+
+
+# Task 2
+# Write a Python script to find out on which day most revenue for users who ordered via a mobile phone was created.
+# Note: a simple print of the result to the console is sufficient
+
+def max_revenue_mobile(cursor):
+    cursor.execute("SELECT datetime FROM Transactions WHERE device_type=3 ORDER BY revenue DESC")
+    return cursor.fetchone()[0]
