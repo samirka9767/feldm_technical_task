3015dcb1970aabdde328f4bdb82263b1ff9ddc3d
Samira <samirehuseyinli@gmail.com>
2022-02-09 14:51:56 +0400

    added test case for task3

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,6 +1,6 @@
 import unittest
 from choose_db import connect_to_db
-# from tasks import
+import os
 from tasks import find_max_revenue, find_max_revenue_mobile
 
 conn = connect_to_db('sqlite')
@@ -18,5 +18,9 @@ class TestTasks(unittest.TestCase):
         actual_value = find_max_revenue_mobile(cursor)
         expected_value = cursor.execute("SELECT datetime, max(revenue) FROM Transactions WHERE device_type=3").fetchone()[0]
         assert actual_value == expected_value
+
+    def test_store_data_into_file(self):
+        is_created = os.path.isfile('./all_transactions.txt')
+        assert is_created
 
 
