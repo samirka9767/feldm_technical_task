66fef4ae2c96ed9c8cdf2cde9b846765831c4750
Samira <samirehuseyinli@gmail.com>
2022-02-09 11:09:15 +0400

    connect to db - choose which db

---
+++ choose_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+import psycopg2
+
+def connect_to_db(config, db_type):
+    #Assuming config is a dictionary with access credentials as such
+    # config = {
+    #     "database": "mydb",
+    #     "user": "postgres",
+    #     "password": "",
+    #     "host": "127.0.0.1",
+    #     "port": "5432"
+    # }
+    if db_type == 'postgres':
+        return psycopg2.connect(database=config["database"], user=config["user"], password=config["password"], host=config["host"], port=config["port"])
+    elif db_type == 'sqlite':
+        connection = sqlite3.connect('db/transactions.db')
+        return connection
