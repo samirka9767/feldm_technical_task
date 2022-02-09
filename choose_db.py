d90736a5c9ba5a6b8de1eb81c88fefcf6d689825
Samira <samirehuseyinli@gmail.com>
2022-02-09 14:32:12 +0400

    add config variables from db.ini

--- choose_db.py
+++ choose_db.py
@@ -1,19 +1,17 @@
 import sqlite3
 import psycopg2
+import configparser
+
 
 #Task 5 - Support Postgres db
 
-def connect_to_db(config, db_type):
-    #Assuming config is a dictionary with access credentials as such
-    # config = {
-    #     "database": "mydb",
-    #     "user": "postgres",
-    #     "password": "",
-    #     "host": "127.0.0.1",
-    #     "port": "5432"
-    # }
+def connect_to_db(db_type):
     if db_type == 'postgres':
-        return psycopg2.connect(database=config["database"], user=config["user"], password=config["password"], host=config["host"], port=config["port"])
+        config = configparser.ConfigParser()
+        config.read('db/db.ini')
+        config = config['postgresql']
+        return psycopg2.connect(database=config["database"], user=config["user"], password=config["password"],
+                                host=config["host"], port=config["port"])
     elif db_type == 'sqlite':
         connection = sqlite3.connect('db/transactions.db')
         return connection
